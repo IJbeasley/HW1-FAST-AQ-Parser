09b342fb81483919d64bd54568eacfbbeee261e5
Isobel Beasley <ibeasley@student.unimelb.edu.au>
2025-01-16 13:05:35 -0800

    Initial unit tests for transcription functions

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -25,11 +25,37 @@ def test_transcribe():
     """
     Write your unit test for the transcribe function here.
     """
-    pass
+
+    seq = "ATACGC"
+
+    assert transcribe(seq, reverse=False) == "UAUGCG"
+    assert transcribe(seq, reverse=True) == "GCGTAT"
+
+    # separate test for lower case - make into function later
+    assert transcribe(seq.lower(), reverse=False) == "UAUGCG"
+    assert transcribe(seq.lower(), reverse=True) == "GCGTAT"
+
+    # test for invalid nucleotide
+    try:
+        transcribe("AZFC")
+    except ValueError as e:
+        assert str(e) == "Invalid nucleotide in sequence. \nOnly allowed sequence values are: ['A', 'C', 'T', 'G']."
 
 
 def test_reverse_transcribe():
     """
     Write your unit test for the reverse transcribe function here.
     """
+
+    assert reverse_transcribe("UAUGCG") == "GCGTAT"
+
+    # lower case test
+    assert reverse_transcribe("uaugcg") == "GCGTAT"
+
+    # test for invalid nucleotide
+    try:
+        reverse_transcribe("AZFC")
+    except ValueError as e:
+        assert str(e) == "Invalid nucleotide in sequence. \nOnly allowed sequence values are: ['A', 'C', 'T', 'G']."
+        
     pass
