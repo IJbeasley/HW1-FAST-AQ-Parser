d7980d161c22d9288b4514cb7cbc2524d3bd8b89
Isobel Beasley <ibeasley@student.unimelb.edu.au>
2025-01-16 12:51:19 -0800

    v2 of transceription functions

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -7,7 +7,7 @@ ALLOWED_NUC = TRANSCRIPTION_MAPPING.keys()
 
 def transcribe(seq: str, reverse: bool = False) -> str:
     """
-    Write a function that will transcribe (replace DNA sequence to RNA
+    A function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
 
     Args:
@@ -21,25 +21,42 @@ def transcribe(seq: str, reverse: bool = False) -> str:
         return reverse_transcribe(seq)
     
     else:
+
         # check that all characters in the input sequence are valid
         if all(nuc in ALLOWED_NUC for nuc in seq):
+            # if nucleotides are valid, then transcrip
             return ''.join(TRANSCRIPTION_MAPPING[nuc] for nuc in seq)
+        
         else:
-            raise ValueError("Invalid nucleotide in sequence. " 
+            raise ValueError("Invalid nucleotide in sequence. \n" 
                                        + "Only allowed sequence values are: " 
                                        + str(list(ALLOWED_NUC))
-                                       + ".")
+                                       + "."
+                                       )
 
 
 def reverse_transcribe(seq: str) -> str:
     """
-    Write a function that will transcribe an input sequence and reverse
+    A function that will transcribe an input sequence and reverse
     the sequence
+
+        Args:
+    seq (str): The DNA sequence to transcribe (e.g. ATACGC).
     """
     # check that all characters in the input sequence are valid
     if all(nuc in ALLOWED_NUC for nuc in seq):
         transcribed_seq =  ''.join(TRANSCRIPTION_MAPPING[nuc] for nuc in seq)[::-1]
         return transcribed_seq
+    
     else: 
-        raise ValueError("Invalid nucleotide in sequence")
-    pass
+        raise ValueError("Invalid nucleotide in sequence. \n" 
+                                  + "Only allowed sequence values are: " 
+                                  + str(list(ALLOWED_NUC))
+                                  + "."
+                                  )
+
+
+print(transcribe("ATACGC")) 
+print(transcribe("ATACGC", reverse=True))
+
+transcribe("AZFC")
